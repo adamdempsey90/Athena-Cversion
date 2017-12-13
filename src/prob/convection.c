f8051c4594783000984779f2b5fced463be3945e
Adam Dempsey <adamdemps@gmail.com>
2017-12-13 14:43:56 -0600

    Added ics for convection, 2D flux bcs.

--- src/prob/convection.c
+++ src/prob/convection.c
@@ -63,6 +63,8 @@ static void reflect_ix3(GridS *pGrid);
 static void reflect_ox3(GridS *pGrid);
 static Real grav_pot2(const Real x1, const Real x2, const Real x3);
 static Real grav_pot3(const Real x1, const Real x2, const Real x3);
+static Real eix(Real x);
+static Real e1xb(Real x); 
 
 /*=========================== PUBLIC FUNCTIONS ===============================*/
 /*----------------------------------------------------------------------------*/
@@ -78,6 +80,9 @@ void problem(DomainS *pDomain)
 #ifdef MHD
   Real b0,angle;
 #endif
+  Real Tval, Pval, Rhoval, Eval;
+  Real T0,T1,P0,P1,Ttop,Ptop;
+  Real Ftot,xi,dlnkdz,g,delad;
   int ixs, jxs, kxs;
 
   is = pGrid->is;  ie = pGrid->ie;
@@ -109,46 +114,123 @@ void problem(DomainS *pDomain)
   angle = (angle/180.)*PI;
 #endif
 
+  dlnkdz = par_getd("problem","dlnkdz");
+  xi = par_getd("problem","xi");
+  Ftot = par_getd("problem","Ftot");
+  g = par_getd("problem","g");
+  loz = par_getd("problem","loz");
+  delta = par_getd("problem","delta");
+
 /* 2D PROBLEM --------------------------------------------------------------- */
 /* Initialize two fluids with interface at y=0.0.  Pressure scaled to give a
  * sound speed of 1 at the interface in the light (lower, d=1) fluid 
  * Perturb V2 using single (iprob=1) or multiple (iprob=2) mode 
  */
 
+
+  delad = 1.-1./Gamma;
+  Ttop = xi/delad;
+  Ptop = xi;
+  T1 = Ttop - log( (1+dlnkdz*loz)/(1+dlnkdz))/dlnkdz; 
+  T0 = T1 + (delad+delta)*(1+2*loz)/delad;
+  P1 = Ptop*exp( (1+dlnkdz)/delad * exp(dlnkdz*Ttop) *( eix(-dlnkdz*T1)-eix(-dlnkdz*Ttop))); 
+  P0 = P1*pow(T0/T1,1./(delad+delta));
+
+  if (pGrid->Nx[1] == 1) {
+  for (k=ks; k<=ke; k++) {
+    for (j=js; j<=je; j++) {
+      for (i=is; i<=ie; i++) {
+        cc_pos(pGrid,i,j,k,&x1,&x2,&x3);
+
+	if (x1 < -loz) {
+	  Tval = T0 + log( (1-dlnkdz*x1)/(1+dlnkdz*loz))/dlnkdz;
+	  Pval = P0*exp( (1+dlnkdz*loz)/delad * exp(-dlnkdz*T0) *( eix(dlnkdz*Tval)-eix(dlnkdz*T0))); 
+
+	}
+	else if (x1 < 1+loz) {
+	  Tval = T1 - g*(1 + delta/delad)*(x1 - (1+loz));
+	  Pval = P1*pow(Tval/T1,1./(delad+delta));
+
+	}
+	else {
+	  Tval = Ttop - log( (1-dlnkdz*(1-x1))/(1+dlnkdz))/dlnkdz;
+	  Pval = Ptop*exp( (1+dlnkdz)/delad * exp(dlnkdz*Ttop) *( eix(-dlnkdz*Tval)-eix(-dlnkdz*Ttop))); 
+
+
+	}
+
+	Dval = Pval/(delad*Tval);
+
+	Eval = Pval/(Gamma-1);
+
+
+
+	pGrid->U[k][j][i].d = Dval;
+        pGrid->U[k][j][i].E = Eval;
+	pGrid->U[k][j][i].M1 = 0.0;
+	pGrid->U[k][j][i].M2 = 0.0;
+	pGrid->U[k][j][i].M3 = 0.0;
+
+	pGrid->U[k][j][i].E+=0.5*SQR(pGrid->U[k][j][i].M1)/pGrid->U[k][j][i].d;
+	pGrid->U[k][j][i].E+=0.5*SQR(pGrid->U[k][j][i].M2)/pGrid->U[k][j][i].d;
+	pGrid->U[k][j][i].E+=0.5*SQR(pGrid->U[k][j][i].M3)/pGrid->U[k][j][i].d;
+	// Potential in E?
+      }
+    }
+  }
+
+/* Enroll gravitational potential to give acceleration in y-direction for 2D
+ * Use special boundary condition routines.  In 2D, gravity is in the
+ * y-direction, so special boundary conditions needed for x2
+*/
+
+  StaticGravPot = grav_pot1;
+  CoolingFunc = AtmosCool1D;
+  if (pDomain->Disp[1] == 0) bvals_mhd_fun(pDomain, left_x1,  reflect_ix1);
+  if (pDomain->MaxX[1] == pDomain->RootMaxX[1])
+    bvals_mhd_fun(pDomain, right_x1, reflect_ox1);
+
+  } /* end of 1D initialization  */
   if (pGrid->Nx[2] == 1) {
   for (k=ks; k<=ke; k++) {
     for (j=js; j<=je; j++) {
       for (i=is; i<=ie; i++) {
         cc_pos(pGrid,i,j,k,&x1,&x2,&x3);
-	pGrid->U[k][j][i].d = 1.0;
-        pGrid->U[k][j][i].E = (1.0/Gamma - x2)/Gamma_1;
+
+	if (x2 < -loz) {
+	  Tval = T0 + log( (1-dlnkdz*x2)/(1+dlnkdz*loz))/dlnkdz;
+	  Pval = P0*exp( (1+dlnkdz*loz)/delad * exp(-dlnkdz*T0) *( expi(dlnkdz*Tval)-expi(dlnkdz*T0))); 
+
+	}
+	else if (x2 < 1+loz) {
+	  Tval = T1 - g*(1 + delta/delad)*(x2 - (1+loz));
+	  Pval = P1*pow(Tval/T1,1./(delad+delta));
+
+	}
+	else {
+	  Tval = Ttop - log( (1-dlnkdz*(1-x2))/(1+dlnkdz))/dlnkdz;
+	  Pval = Ptop*exp( (1+dlnkdz)/delad * exp(dlnkdz*Ttop) *( expi(-dlnkdz*Tval)-expi(-dlnkdz*Ttop))); 
+
+
+	}
+
+	Dval = Pval/(delad*Tval);
+
+	Eval = Pval/(Gamma-1);
+
+
+
+	pGrid->U[k][j][i].d = Dval;
+        pGrid->U[k][j][i].E = Eval;
 	pGrid->U[k][j][i].M1 = 0.0;
-        if (iprob == 1) {
-          pGrid->U[k][j][i].M2 = amp/4.0*
-            (1.0+cos(2.0*PI*x1/lx))*(1.0+cos(2.0*PI*x2/ly));
-        }
-        else {
-          pGrid->U[k][j][i].M2 = amp*(ran2(&iseed) - 0.5)*
-            (1.0+cos(2.0*PI*x2/ly));
-	}
-        pGrid->U[k][j][i].M1 = 0.0;
-        pGrid->U[k][j][i].M2 = 0.0;
-        pGrid->U[k][j][i].M3 = 0.0;
-        if (x2 > 0.0) {
-	  pGrid->U[k][j][i].d = 2.0;
-          pGrid->U[k][j][i].M2 *= 2.0;
-          pGrid->U[k][j][i].E = (1.0/Gamma - x2)/Gamma_1;
-	}
+	pGrid->U[k][j][i].M2 = 0.0;
+	pGrid->U[k][j][i].M3 = 0.0;
+
+	pGrid->U[k][j][i].E+=0.5*SQR(pGrid->U[k][j][i].M1)/pGrid->U[k][j][i].d;
 	pGrid->U[k][j][i].E+=0.5*SQR(pGrid->U[k][j][i].M2)/pGrid->U[k][j][i].d;
-#ifdef MHD
-	pGrid->B1i[k][j][i] = b0;
-	pGrid->U[k][j][i].B1c = b0;
-        pGrid->U[k][j][i].E += 0.5*b0*b0;
-#endif
+	pGrid->U[k][j][i].E+=0.5*SQR(pGrid->U[k][j][i].M3)/pGrid->U[k][j][i].d;
+	// Potential in E?
       }
-#ifdef MHD
-    pGrid->B1i[k][j][ie+1] = b0;
-#endif
     }
   }
 
@@ -180,85 +262,38 @@ void problem(DomainS *pDomain)
     for (j=js; j<=je; j++) {
       for (i=is; i<=ie; i++) {
         cc_pos(pGrid,i,j,k,&x1,&x2,&x3);
-	pGrid->U[k][j][i].d = 1.0;
-        pGrid->U[k][j][i].E = (1.0/Gamma - 0.1*x3)/Gamma_1;
+	if (x3 < -loz) {
+	  Tval = T0 + log( (1-dlnkdz*x3)/(1+dlnkdz*loz))/dlnkdz;
+	  Pval = P0*exp( (1+dlnkdz*loz)/delad * exp(-dlnkdz*T0) *( expi(dlnkdz*Tval)-expi(dlnkdz*T0))); 
+
+	}
+	else if (x3 < 1+loz) {
+	  Tval = T1 - g*(1 + delta/delad)*(x3 - (1+loz));
+	  Pval = P1*pow(Tval/T1,1./(delad+delta));
+
+	}
+	else {
+	  Tval = Ttop - log( (1-dlnkdz*(1-x3))/(1+dlnkdz))/dlnkdz;
+	  Pval = Ptop*exp( (1+dlnkdz)/delad * exp(dlnkdz*Ttop) *( expi(-dlnkdz*Tval)-expi(-dlnkdz*Ttop))); 
+
+
+	}
+
+	Dval = Pval/(delad*Tval);
+
+	Eval = Pval/(Gamma-1);
+
+
+
+	pGrid->U[k][j][i].d = Dval;
+        pGrid->U[k][j][i].E = Eval;
 	pGrid->U[k][j][i].M1 = 0.0;
 	pGrid->U[k][j][i].M2 = 0.0;
-        if (iprob == 1) {
-          pGrid->U[k][j][i].M3 = amp/8.0*(1.0+cos(2.0*PI*x1/lx))*
-            (1.0+cos(2.0*PI*x2/ly))*(1.0+cos(2.0*PI*x3/lz));
-        }
-        else {
-          pGrid->U[k][j][i].M3 = amp*(ran2(&iseed) - 0.5)*
-            (1.0+cos(2.0*PI*x3/lz));
-	}
-        if (x3 > 0.0) {
-	  pGrid->U[k][j][i].d = rhoh;
-          pGrid->U[k][j][i].M3 *= rhoh;
-          pGrid->U[k][j][i].E = (1.0/Gamma - 0.1*rhoh*x3)/Gamma_1;
-	}
-	pGrid->U[k][j][i].E+=0.5*SQR(pGrid->U[k][j][i].M3)/pGrid->U[k][j][i].d;
-#ifdef MHD
-        switch(iprob){
-        case 3: /* B only in light fluid, do not add B^2 to E, total P const */
-          if (x3 <= 0.0) {
-            pGrid->B1i[k][j][i] = b0;
-            if (i == ie) pGrid->B1i[k][j][ie+1] = b0;
-            pGrid->U[k][j][i].B1c = b0;
-          }
-          break;
-        case 4: /* discontinuous rotation of B by angle at interface */
-          if (x3 <= 0.0) {
-            pGrid->B1i[k][j][i] = b0;
-            if (i == ie) pGrid->B1i[k][j][ie+1] = b0;
-            pGrid->U[k][j][i].B1c = b0;
-            pGrid->U[k][j][i].E += 0.5*b0*b0;
-          }
-          else {
-            pGrid->B1i[k][j][i] = b0*cos(angle);
-            pGrid->B2i[k][j][i] = b0*sin(angle);
-            if (i == ie) pGrid->B1i[k][j][ie+1] = b0*cos(angle);
-            if (j == je) pGrid->B2i[k][je+1][i] = b0*sin(angle);
-            pGrid->U[k][j][i].B1c = b0*cos(angle);
-            pGrid->U[k][j][i].B2c = b0*sin(angle);
-            pGrid->U[k][j][i].E += 0.5*b0*b0;
-          }
-          break;
-        case 5: /* rotation of B by angle over distance L_rot at interface */
-          if (x3 <= (-L_rot/2.0)) {
-            pGrid->B1i[k][j][i] = b0;
-            if (i == ie) pGrid->B1i[k][j][ie+1] = b0;
-            pGrid->U[k][j][i].B1c = b0;
-            pGrid->U[k][j][i].E += 0.5*b0*b0;
-          }
-          else if (x3 >= (L_rot/2.0)) {
-            pGrid->B1i[k][j][i] = b0*cos(angle);
-            pGrid->B2i[k][j][i] = b0*sin(angle);
-            if (i == ie) pGrid->B1i[k][j][ie+1] = b0*cos(angle);
-            if (j == je) pGrid->B2i[k][je+1][i] = b0*sin(angle);
-            pGrid->U[k][j][i].B1c = b0*cos(angle);
-            pGrid->U[k][j][i].B2c = b0*sin(angle);
-            pGrid->U[k][j][i].E += 0.5*b0*b0;
-          }
-          else {
-            fact = ((L_rot/2.0)+x3)/L_rot;
-            pGrid->B1i[k][j][i] = b0*cos(fact*angle);
-            pGrid->B2i[k][j][i] = b0*sin(fact*angle);
-            if (i == ie) pGrid->B1i[k][j][ie+1] = b0*cos(fact*angle);
-            if (j == je) pGrid->B2i[k][je+1][i] = b0*sin(fact*angle);
-            pGrid->U[k][j][i].B1c = b0*cos(fact*angle);
-            pGrid->U[k][j][i].B2c = b0*sin(fact*angle);
-            pGrid->U[k][j][i].E += 0.5*b0*b0;
-          }
+	pGrid->U[k][j][i].M3 = 0.0;
 
-          break;
-        default:
-          pGrid->B1i[k][j][i] = b0;
-          if (i == ie) pGrid->B1i[k][j][ie+1] = b0;
-          pGrid->U[k][j][i].B1c = b0;
-          pGrid->U[k][j][i].E += 0.5*b0*b0;
-        }
-#endif
+	pGrid->U[k][j][i].E+=0.5*SQR(pGrid->U[k][j][i].M1)/pGrid->U[k][j][i].d;
+	pGrid->U[k][j][i].E+=0.5*SQR(pGrid->U[k][j][i].M2)/pGrid->U[k][j][i].d;
+	pGrid->U[k][j][i].E+=0.5*SQR(pGrid->U[k][j][i].M3)/pGrid->U[k][j][i].d;
       }
     }
   }
@@ -428,55 +463,48 @@ double ran2(long int *idum)
 
 /*----------------------------------------------------------------------------*/
 /*! \fn static void reflect_ix2(GridS *pGrid)
- *  \brief Special reflecting boundary functions in x2 for 2D sims
+ *  \brief constant flux, no-slip wall b.c 
  */
 
-static void reflect_ix2(GridS *pGrid)
+static void flux_ix2(GridS *pGrid)
 {
   int js = pGrid->js;
   int ks = pGrid->ks, ke = pGrid->ke;
   int i,j,k,il,iu,ku; /* i-lower/upper;  k-upper */
+  Real Tval, Pval, Rhoval
+  Real delad = 1. - 1./Gamma;
+	Real x1,x2,x3,x10,x20,x30;
+  Real dlnkdz = 3.5;
 
   iu = pGrid->ie + nghost;
   il = pGrid->is - nghost;
 
-  for (k=ks; k<=ke; k++) {
-    for (j=1; j<=nghost; j++) {
-      for (i=il; i<=iu; i++) {
-        pGrid->U[k][js-j][i]    =  pGrid->U[k][js+(j-1)][i];
-        pGrid->U[k][js-j][i].M2 = -pGrid->U[k][js-j][i].M2; /* reflect 2-mom. */
-        pGrid->U[k][js-j][i].E +=  
-	  pGrid->U[k][js+(j-1)][i].d*0.1*(2*j-1)*pGrid->dx2/Gamma_1;
-      }
-    }
-  }
-
-#ifdef MHD
-  for (k=ks; k<=ke; k++) {
-    for (j=1; j<=nghost; j++) {
-      for (i=il; i<=iu; i++) {
-        pGrid->B1i[k][js-j][i] = pGrid->B1i[k][js+(j-1)][i];
-      }
-    }
-  }
+  cc_pos(pGrid,1,js,1,&x10,&x20,&x30);
 
   for (k=ks; k<=ke; k++) {
     for (j=1; j<=nghost; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->B2i[k][js-j][i] = pGrid->B2i[k][js+(j-1)][i];
+        cc_pos(pGrid,i,j,k,&x1,&x2,&x3);
+
+				D0 = pGrid->U[k][js][i].d;				
+				E0 = pGrid->U[k][js][i].E;				
+				P0 = (Gamma-1)*(E0 - .5*(SQR(pGrid->U[k][js][i].M1) SQR(pGrid->U[k][js][i].M2)-SQR(pGrid->U[k][js][i].M3))/D0);
+				T0 = P0/(delad*D0);
+
+				Tval = T0 + log((1-dlnkdz*x2)/(1-dlnkdz*x20))/dlnkdz; // Exact conductive T(z) 
+				Pval = P0*exp( (1-dlnkdz*x20)/delad*exp(-dlnkdz*T0)*(eix(dlnkdz*Tval)-eix(dlnkdz*T0))); // Exact hydrostatic pressure for T(z)
+				Rhoval = Pval/(delad*Tval);
+
+				pGrid->U[k][js-j][i].d = Rhoval; 
+				pGrid->U[k][js-j][i].M1 = pGrid->U[k][js+(j-1)][i].M1;	// Zero gradient in horizontal velocities (stress free)	
+				pGrid->U[k][js-j][i].M2 = -pGrid->U[k][js+(j-1)][i].M2;	// Zero vertical velocity	
+				pGrid->U[k][js-j][i].d = Pval/(Gamma-1) + .5/Rhoval * (SQR(pGrid->U[k][js-j][i].M1) + SQR(pGrid->U[k][js-j][i].M2)+SQR(pGrid->U[k][js-j][i].M3));
+				
       }
     }
   }
 
-  if (pGrid->Nx[2] > 1) ku=ke+1; else ku=ke;
-  for (k=ks; k<=ku; k++) {
-    for (j=1; j<=nghost; j++) {
-      for (i=il; i<=iu; i++) {
-        pGrid->B3i[k][js-j][i] = pGrid->B3i[k][js+(j-1)][i];
-      }
-    }
-  }
-#endif
+
 
   return;
 }
@@ -491,6 +519,10 @@ static void reflect_ox2(GridS *pGrid)
   int je = pGrid->je;
   int ks = pGrid->ks, ke = pGrid->ke, ku;
   int i,j,k,il,iu,jl,ju; /* i/j-lower/upper */
+  Real Tval, Pval, Rhoval
+  Real delad = 1. - 1./Gamma;
+	Real x1,x2,x3,x10,x20,x30;
+  Real dlnkdz = 3.5;
 
   iu = pGrid->ie + nghost;
   il = pGrid->is - nghost;
@@ -503,44 +535,32 @@ static void reflect_ox2(GridS *pGrid)
     jl = pGrid->js;
   }
 
+  cc_pos(pGrid,1,je,1,&x10,&x20,&x30);
+	x20 = 1-x20;
   for (k=ks; k<=ke; k++) {
     for (j=1; j<=nghost; j++) {
       for (i=il; i<=iu; i++) {
-        pGrid->U[k][je+j][i]    =  pGrid->U[k][je-(j-1)][i];
-        pGrid->U[k][je+j][i].M2 = -pGrid->U[k][je+j][i].M2; /* reflect 2-mom. */
-        pGrid->U[k][je+j][i].E -=
-          pGrid->U[k][je-(j-1)][i].d*0.1*(2*j-1)*pGrid->dx2/Gamma_1;
+        cc_pos(pGrid,i,j,k,&x1,&x2,&x3);
+				x2 = 1.-x2;
+
+				D0 = pGrid->U[k][je][i].d;				
+				E0 = pGrid->U[k][je][i].E;				
+				P0 = (Gamma-1)*(E0 - .5*(SQR(pGrid->U[k][je][i].M1) SQR(pGrid->U[k][je][i].M2)-SQR(pGrid->U[k][je][i].M3))/D0);
+				T0 = P0/(delad*D0);
+
+				Tval = T0 - log((1-dlnkdz*x2)/(1-dlnkdz*x20))/dlnkdz; // Exact conductive T(z) 
+				Pval = P0*exp( (1-dlnkdz*x20)/delad*exp(dlnkdz*T0)*(eix(-dlnkdz*Tval)-eix(-dlnkdz*T0))); // Exact hydrostatic pressure for T(z)
+				Rhoval = Pval/(delad*Tval);
+
+				pGrid->U[k][je+j][i].d = Rhoval; 
+				pGrid->U[k][je+j][i].M1 = pGrid->U[k][je-(j-1)][i].M1;	// Zero gradient in horizontal momenta (stress free)	
+				pGrid->U[k][je+j][i].M2 = -pGrid->U[k][je-(j-1)][i].M2;	// Zero vertical momentum 
+				pGrid->U[k][je+j][i].d = Pval/(Gamma-1) + .5/Rhoval * (SQR(pGrid->U[k][je+j][i].M1) + SQR(pGrid->U[k][je+j][i].M2)+SQR(pGrid->U[k][je+j][i].M3));
       }
     }
   }
 
-#ifdef MHD
-  for (k=ks; k<=ke; k++) {
-    for (j=1; j<=nghost; j++) {
-      for (i=il; i<=iu; i++) {
-        pGrid->B1i[k][je+j][i] = pGrid->B1i[k][je-(j-1)][i];
-      }
-    }
-  }
 
-/* j=je+1 is not set for the interface field B2i */
-  for (k=ks; k<=ke; k++) {
-    for (j=2; j<=nghost; j++) {
-      for (i=il; i<=iu; i++) {
-        pGrid->B2i[k][je+j][i] = pGrid->B2i[k][je-(j-2)][i];
-      }
-    }
-  }
-
-  if (pGrid->Nx[2] > 1) ku=ke+1; else ku=ke;
-  for (k=ks; k<=ku; k++) {
-    for (j=1; j<=nghost; j++) {
-      for (i=il; i<=iu; i++) {
-        pGrid->B3i[k][je+j][i] = pGrid->B3i[k][je-(j-1)][i];
-      }
-    }
-  }
-#endif
 
   return;
 }
@@ -666,17 +686,91 @@ static void reflect_ox3(GridS *pGrid)
 
 /*----------------------------------------------------------------------------*/
 /*! \fn static Real grav_pot2(const Real x1, const Real x2, const Real x3)
- *  \brief Gravitational potential; g = 0.1
+ *  \brief Gravitational potential; g = 1.0
  */
 
+static Real grav_pot1(const Real x1, const Real x2, const Real x3)
+{
+  return x1;
+}
 static Real grav_pot2(const Real x1, const Real x2, const Real x3)
 {
-  return 0.1*x2;
+  return x2;
 }
 /*! \fn static Real grav_pot3(const Real x1, const Real x2, const Real x3)
  *  \brief Gravitational potential; g = 0.1
  */
 static Real grav_pot3(const Real x1, const Real x2, const Real x3)
 {
-  return 0.1*x3;
+  return x3;
+}
+
+
+static Real e1xb(Real x) {
+  int maxit;
+  Real eps,euler,fpmin,fpmax;
+  eps=6e-8;
+  euler=.57721566;
+  maxit=20;
+  fpmin=1e-30;
+  fpmax=1e30;
+  int k;
+  Real sum,term;
+
+  if (x == 0.0) return fpmax;
+  if (x <= 1.) {
+    sum = 1.;
+    term = 1.;
+    k=1;
+    while ((fabs(term)>fabs(sum)*eps)&&(k<=maxit)){
+      term = -term*k*x/( (k+1.)*(k+1.));
+      sum = sum + term;
+      k++;
+    }
+    return -euler - log(x) + x*sum;
+  }
+
+  term = 0.;
+  for (k=maxit+(int)floor(80/x);k>=1;k--) {
+    term = k/(1. + k/(x+term));
+  }
+  return exp(-x) / (x+term);
+}
+
+static Real eix(Real x) {
+  int maxit;
+  Real eps,euler,fpmin,fpmax;
+  eps=6e-8;
+  euler=.57721566;
+  maxit=100;
+  fpmin=1e-30;
+  fpmax=1e30;
+  int k;
+  Real sum,term;
+  if (x == 0.0) return -fpmax;
+  
+  if (x < 0.0) return -de1xb(-x);
+  if (fabs(x) <= 40.) {
+    // Power series around x=0
+  
+    sum = 1.0;
+    term =1.0;
+    k = 1;
+    while ((fabs(term/sum) > eps)&&(k<=maxit)) {
+      term = term*k*x/( (k+1.)*(k+1.));
+      sum += term;
+      k++;
+    }
+    return euler + log(x) + x*sum;
+  }
+  
+  // Asymptotic expansion (the series is not convergent)
+  sum = 1.0;
+  term = 1.0;
+  
+  for (k=1;k<=20;k++) {
+    term = term*k/x;
+    sum += term;
+  }
+  return exp(x)/x * sum;
 }
